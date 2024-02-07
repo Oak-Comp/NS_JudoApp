91fcc00253bb54f53ac063838630bf75e1d856fb
Derek Peacock <derek.peacock@yahoo.com>
2024-02-07 14:46:54 +0000

    Add project files.

---
+++ NS_JudoApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NS_JudoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
