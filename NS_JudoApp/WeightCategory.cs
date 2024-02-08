2e828fa87f12c3738c2fda4b1224d7ec701d1263
Derek Peacock <derek.peacock@yahoo.com>
2024-02-08 10:38:41 +0000

    Added weight Categories

---
+++ NS_JudoApp/WeightCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace NS_JudoApp
+{
+    public enum WeightCategory
+    {
+        Flyweight = 66,
+        Lightweight = 73,
+        Light_Middleweight = 81,
+        Middleweight = 90,
+        Light_Heavyweight = 100,
+        Heavyweight = 101
+    }
+}
